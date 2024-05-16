e8f89d39b331389819f631e07b0bb3e8496cf85a
AbnaEV <abnaev786@gmail.com>
2024-05-16 22:12:52 +0530

    update search page

--- src/HydrotrackSearchPage.js
+++ src/HydrotrackSearchPage.js
@@ -1,36 +1,44 @@
 import React, { useState } from 'react';
 import './map.css'; // Import your CSS file
 import DropdownMenu from './DropdownMenu';
+import MapComponent from './MapComponent'; // Adjust the path as necessary
 
 function HydrotrackSearchPage() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const redirectToOpenStreetMap = () => {
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
     if (searchTerm.trim() !== '') {
-      const openStreetMapUrl = `https://www.openstreetmap.org/search?query=${encodeURIComponent(searchTerm)}`;
-      window.location.href = openStreetMapUrl;
+      setSubmitted(true);
     } else {
-      alert('Please enter a location to search on OpenStreetMap.');
+      alert('Please enter a location to search on the map.');
     }
   };
 
   return (
-    <div className="map-button-container">
+    <div className="map-container">
       <DropdownMenu />
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleSearchChange}
-        placeholder="Enter location"
-        className="map-input"
-      />
-      <button onClick={redirectToOpenStreetMap} className="map-button">Search on OpenStreetMap</button>
+      <h1>Water Resources in Kerala</h1>
+      <form onSubmit={handleSearchSubmit} className="map-form">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Enter location"
+          className="map-input"
+        />
+        <button type="submit" className="map-button">Search on Map</button>
+      </form>
+      {submitted && <MapComponent searchTerm={searchTerm} />}
     </div>
   );
 }
 
 export default HydrotrackSearchPage;
+
+
